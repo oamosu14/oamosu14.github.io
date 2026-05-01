f4b12dfe0e10b2aeedc5124ff2de9fdb186adb2b
oamosu14 <65821271+oamosu14@users.noreply.github.com>
2026-04-30 23:50:13 -0400

    Create main.js

---
+++ assets/main.js
@@ -0,0 +1,13 @@
+// =============================
+// NAVBAR SHRINK ON SCROLL
+// =============================
+
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+
+  if (window.scrollY > 50) {
+    navbar.classList.add("shrink");
+  } else {
+    navbar.classList.remove("shrink");
+  }
+});
